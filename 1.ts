aca284ec913d4c6be3729672689b82642cd05df6
PolinaSloboda <sloboda.polina@gmail.com>
2019-04-18 17:30:20 +0300

    функции равенства

--- 1.ts
+++ 1.ts
@@ -1,15 +1,35 @@
 // описывает философа
 class phy {
-  // взяты ли левая и правая вилки
-  constructor(public left: boolean, public right: boolean) {}
+    // взяты ли левая и правая вилки
+    constructor(public left: boolean, public right: boolean) {}
+    equals(another: phy) {
+        return this.left === another.left && this.right === another.right;
+    }
 }
 
 // состояние системы. определяется вилками в руках философов
 class state {
-  // phys - массив философов
-  constructor(public phys: phy[]) {}
-}
+    // phys - массив философов
+    constructor(public phys: phy[]) {}
 
+    equals(another: state) {
+        let length = this.phys.length;
+        if (length !== another.phys.length) {
+            return false;
+        }
+
+        for(let i = 0; i < length; i++) {
+            // ищем с i-м сдвигом полное совпадение состояния философов.
+            // если на одном из сдвигов нашли - супер.
+            if (this.phys.find(function(element, index) {
+                    return !element.equals(another.phys[(index + i) % length]);
+                }) === undefined // проверка, что не нашлось отличающегося философа
+            ) { return true; }
+        }
+        return false;
+    }
+
+}
 
 
 
